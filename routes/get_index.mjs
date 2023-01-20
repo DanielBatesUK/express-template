6a2078c02a6f86051ffa55360a053c377b765879
DanielBatesUK <email@danielbates.co.uk>
2023-01-20 16:48:37 +0000

    Updated index pug and logs

--- routes/get_index.mjs
+++ routes/get_index.mjs
@@ -8,15 +8,15 @@
 // Route - Index
 export default function routeIndex(req, res) {
   try {
-    console.log(`HTTP ${req.method} request for '${req.path}' as 'index' processing`);
+    console.log(`'get_index': HTTP ${req.method} request for '${req.path}' processing`);
     res.status(200).render('index');
     res.end();
-    console.log(`HTTP ${req.method} request for '${req.path}' as 'index' completed`);
+    console.log(`'get_index': HTTP ${req.method} request for '${req.path}' completed`);
   } catch (error) {
-    console.error(`HTTP ${req.method} request for '${req.path}' as 'index' failed`);
-    console.error(`'index' error: ${error.message}`);
+    console.error(`'get_index': HTTP ${req.method} request for '${req.path}' failed`);
+    console.error(`'get_index': error: ${error.message}`);
     console.error(error);
-    res.status(500).send(`Index error`);
+    res.status(500).send(`'get_index': error`);
     res.end();
   }
 }
