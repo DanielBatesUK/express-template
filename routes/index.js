4130d1440697dfdeb78f055663a47ab1fc8e5bd9
DanielBatesUK <email@danielbates.co.uk>
2023-07-04 00:18:56 +0100

    Updated

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,35 @@
 // ################################################################################################
 
+// Imports
+import express from 'express';
+
 // My Imports
-import logTime from '../lib/log_time.mjs';
+import logTime from '../lib/log_time.js';
+
+// ################################################################################################
+
+// Express
+const router = express.Router();
 
 // ################################################################################################
 
 // Route - Index
-export default function routeIndex(req, res) {
+router.get('/', (req, res) => {
   try {
     console.log(`${logTime()} Processing HTTP ${req.method} request for '${req.path}' with 'get_index'...`);
     res.status(200).render('index');
     res.end();
     console.log(`${logTime()} Completed HTTP ${req.method} request for '${req.path}' with 'get_index'`);
   } catch (error) {
-    console.error(`${logTime()} 'get_index' error...`, error.message);
-    res.status(500).json({ error: `'get_index' error` });
+    console.error(`${logTime()} Get index error...`, error.message);
+    res.status(500).json({ error: 'Get Index error' });
     res.end();
   }
-}
+});
+
+// ################################################################################################
+
+// Exports
+export default router;
 
 // ################################################################################################
